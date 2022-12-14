eca8ea61fbe2fde65c806429d08361185ce6d7f1
Vishal Kumar <vy10607@gmail.com>
2022-12-14 13:14:10 +0530

    Lecture 8 Completed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,16 +8,51 @@ function TextUtils(props) {
     setText(newText)
   }
 
+  let convertToLowercase = () => {
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+
+  let reverseText = () => {
+    let newText = text.split("").reverse().join("");
+    setText(newText);
+  }
+
+  let clearAll = () => {
+    setText("");
+  }
+
   let handleOnChange = (event) => {
     setText(event.target.value);
   }
 
+  let words = 0;
+  let arrOfWords = text.split(" ");
+  
+  for (let word of arrOfWords){
+    if (word !== ""){
+      words += 1;
+    }
+  }
+
   return (
-    <div className="container mt-4">
-        <h2>{props.heading}</h2>
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="12"></textarea>
+    <>
+    <div className="container mt-4 mb-3">
+        <h1>{props.heading}</h1>
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
         <button className="btn btn-primary mt-3" onClick={convertToUppercase}>Convert to Uppercase</button>
+        <button className="btn btn-primary mt-3 mx-2" onClick={convertToLowercase}>Convert to Lowercase</button>
+        <button className="btn btn-primary mt-3" onClick={reverseText}>Reverse Text</button>
+        <button className="btn btn-primary mt-3 mx-2" onClick={clearAll}>Clear All</button>
     </div>
+    <div className="container">
+      <h2>Your text summary</h2>
+      <p>{words} words and {text.length} characters</p>
+      <p>{0.008 * words} minutes read</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
+    </div>
+  </>
   )
 }
 
