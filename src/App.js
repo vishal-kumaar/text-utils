08f4f6fbf4bcccc8d147df29062d6de3e228b7d9
Vishal Kumar <vy10607@gmail.com>
2022-12-14 20:02:23 +0530

    Lecture 10

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 
 function App() {
   return (
     <>
     <Navbar title="TextUtils"/>
     <TextForm heading = "Enter to analyze the text" />
+    <About/>
     </>
   );
 }
